d635a18a46cf02c5468943b04b024b725535947e
zzinnykko <zzinnykko@outlook.com>
2026-01-31 14:36:22 +0900

    init astro

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        UnoCSS(),
+    ],
+});
