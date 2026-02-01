88fc172280a6b26240f4d6b4adfe5d536b94795c
zzinnykko <zzinnykko@outlook.com>
2026-02-01 13:31:51 +0900

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
