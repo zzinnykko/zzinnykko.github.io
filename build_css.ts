6c06eea94cea890e72e0a2d680bb3fb3f00d9222
zzinnykko <zzinnykko@outlook.com>
2026-02-11 18:09:56 +0900

    f

--- build_css.ts
+++ build_css.ts
@@ -1,7 +1,7 @@
 import fs from "fs-extra";
 import fg from "fast-glob";
-import { createGenerator } from "unocss";
-import defineConfig from "$/uno.config.ts";
+import postcss from "postcss";
+import tailwindcss from "@tailwindcss/postcss";
 
 
 const srcs = await fg.glob(["./**/*.tsx"]);
@@ -9,8 +9,11 @@ const codes = await Promise.all(
     srcs.map(async (src) => await fs.readFile(src, { encoding: "utf-8" }))
 );
 
-const uno = await createGenerator(defineConfig);
-const { css } = await uno.generate(await fs.readFile("./resources/source.css", { encoding: "utf-8" }), {
-    id: "source.css"
-});
-await fs.outputFile("./_site/global.css", css, { encoding: "utf-8" });
+await postcss([tailwindcss()]).process()
+
+
+// const uno = await createGenerator(defineConfig);
+// const { css } = await uno.generate(await fs.readFile("./resources/source.css", { encoding: "utf-8" }), {
+//     id: "source.css"
+// });
+// await fs.outputFile("./_site/global.css", css, { encoding: "utf-8" });
